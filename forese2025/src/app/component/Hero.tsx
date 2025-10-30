931d8a171b1bc7bcb863e3056b54184dc8c726b6
Thamizh selvan <143354164+THAMIZHSELVAN08@users.noreply.github.com>
2025-10-30 22:26:44 +0530

    Update Hero.tsx

--- forese2025/src/app/component/Hero.tsx
+++ forese2025/src/app/component/Hero.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { MdArrowOutward } from "react-icons/md";
+import { motion } from "framer-motion";
 import TextType from "@/components/ui/TextType";
 
 export default function Hero() {
@@ -17,14 +18,14 @@ export default function Hero() {
             FOR{" "}
           </h1>
           <h1
-            className="text-white"
+            className="text-white text-center"
             style={{ fontFamily: "var(--font-hanken-grotesk)" }}
           >
             um for Economic Studies
           </h1>
         </span>
         <h1
-          className="text-white"
+          className="text-white text-center"
           style={{ fontFamily: "var(--font-hanken-grotesk)" }}
         >
           By Engineers
@@ -42,12 +43,20 @@ export default function Hero() {
           className="text-white text-xl mt-5 text-center"
         />
         <Link href="/Team">
-          <button className="bg-white text-xl font-medium text-blue-800 rounded-md px-4 py-2 mt-10 hover:bg-blue-800 hover:text-white tracking-tight transition-colors duration-200">
-            <span className="inline-flex items-center">
+               <motion.button
+                   className="bg-white text-xl font-medium text-blue-800 rounded-md px-4 py-2 mt-10 
+                   tracking-tight transition-all duration-300 shadow-md hover:bg-blue-800 
+                   hover:text-white hover:shadow-xl cursor-pointer flex items-center justify-center"
+          whileHover={{
+               scale: 1.05,
+               y: -3,
+               boxShadow: "0 8px 25px rgba(30, 64, 175, 0.3)", // soft blue glow
+                      }}
+                 whileTap={{ scale: 0.95 }}
+             >
               Know the team
-              <MdArrowOutward className="ml-2" />
-            </span>
-          </button>
+            <MdArrowOutward className="ml-2" />
+                </motion.button>
         </Link>
       </div>
 
