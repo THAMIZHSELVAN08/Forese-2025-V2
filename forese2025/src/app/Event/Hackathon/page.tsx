83806ef7a68ec0e9a7c78e2479ed6980941de8e3
Thamizh selvan <143354164+THAMIZHSELVAN08@users.noreply.github.com>
2025-10-30 18:54:38 +0530

    Update page.tsx

--- forese2025/src/app/Event/Hackathon/page.tsx
+++ forese2025/src/app/Event/Hackathon/page.tsx
@@ -182,7 +182,6 @@ const HackathonPage = () => {
         </div>
       </section>
 
-      {/* Back to Events Button */}
       <div className="fixed bottom-6 right-6 z-50">
         <Link href="/Event">
           <motion.button
